7f3c1efc4f9a644219d03bddddaed13dfb3321c5
Cong Shen <shenc0411@gmail.com>
2019-06-26 20:28:48 -0700

    Added camera struct

--- Assets/Scripts/Structs.cs
+++ Assets/Scripts/Structs.cs
@@ -22,4 +22,26 @@
         }
     }
 
+    public struct TDCamera
+    {
+        float3 forward;
+        float3 up;
+        float3 right;
+        float3 position;
+        int2 pixelResolution;
+        float aspectRatio; // width / height
+        float verticalFOV;
+
+        public TDCamera(Camera camera)
+        {
+            this.position = camera.transform.position;
+            this.forward = camera.transform.forward;
+            this.up = camera.transform.up;
+            this.right = camera.transform.right;
+            this.pixelResolution.x = camera.pixelWidth;
+            this.pixelResolution.y = camera.pixelHeight;
+            this.aspectRatio = camera.aspect;
+            this.verticalFOV = camera.fieldOfView;
+        }
+    }
 }
--- Assets/Scripts/TDRenderer.cs
+++ Assets/Scripts/TDRenderer.cs
@@ -6,17 +6,7 @@
 
     public class TDRenderer : MonoBehaviour
     {
-        // Start is called before the first frame update
-        void Start()
-        {
-
-        }
-
-        // Update is called once per frame
-        void Update()
-        {
-
-        }
+        
     }
 
 }
