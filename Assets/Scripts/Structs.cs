d2bd550d9df6587d4e29c863df84f5af8cdcd433
Cong Shen <shenc0411@gmail.com>
2019-06-26 20:20:06 -0700

    Added base structures

---
+++ Assets/Scripts/Structs.cs
@@ -0,0 +1,25 @@
+﻿namespace TorchDragon
+{
+    using System.Collections;
+    using System.Collections.Generic;
+    using Unity.Mathematics;
+    using UnityEngine;
+
+    public struct TDRay
+    {
+        float3 origin;
+        float3 direction;
+
+        public TDRay(float3 origin, float direction)
+        {
+            this.origin = origin;
+            this.direction = direction;
+        }
+
+        public float3 PointAtParameter(float t)
+        {
+            return origin + direction * t;
+        }
+    }
+
+}
---
+++ Assets/Scripts/TDRenderer.cs
@@ -0,0 +1,22 @@
+﻿namespace TorchDragon
+{
+    using System.Collections;
+    using System.Collections.Generic;
+    using UnityEngine;
+
+    public class TDRenderer : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+
+}
